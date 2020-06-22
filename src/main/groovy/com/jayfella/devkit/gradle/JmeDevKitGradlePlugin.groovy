f75c957f3d9a0b7488cccf539ac1ce8b6b46886a
jayfella <jayfella@gmail.com>
2020-06-22 13:28:54 +0100

    Configure for java 8 compatibility.

--- src/main/groovy/com/jayfella/devkit/gradle/JmeDevKitGradlePlugin.groovy
+++ src/main/groovy/com/jayfella/devkit/gradle/JmeDevKitGradlePlugin.groovy
@@ -1,11 +1,12 @@
 package com.jayfella.devkit.gradle
 
+import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.Dependency
-import org.gradle.api.artifacts.ExternalModuleDependency
 import org.gradle.api.tasks.JavaExec
 import org.gradle.api.tasks.compile.JavaCompile
+import org.gradle.internal.jvm.Jvm
 
 class JmeDevKitGradlePlugin implements Plugin<Project> {
 
@@ -37,7 +38,6 @@ class JmeDevKitGradlePlugin implements Plugin<Project> {
                 apply('java')
                 apply('application')
             }
-
         }
     }
 
@@ -55,7 +55,6 @@ class JmeDevKitGradlePlugin implements Plugin<Project> {
      */
     private void addRepositories() {
         project.repositories {
-            mavenLocal()
             jcenter()
         }
     }
@@ -72,8 +71,24 @@ class JmeDevKitGradlePlugin implements Plugin<Project> {
     }
 
     private void configureDependencies() {
-        Dependency dep = project.dependencies.create("com.jayfella:jme-jfx-devkit:0.0.1")
+
+        // include the devkit as a dependency
+        Dependency dep = project.dependencies.create("com.jayfella:jme-jfx-devkit:0.0.2")
         project.dependencies.add("runtimeOnly", dep);
+
+        // if we're not using java 8 we must include javafx
+        if (Jvm.current().javaVersion > JavaVersion.VERSION_1_8) {
+
+            project.with {
+                plugins.with {
+                    apply({ 'org.openjfx.javafxplugin:0.0.8' })
+                }
+            }
+
+            Dependency jfxDep = project.dependencies.create("org.openjfx:javafx:14.0.1")
+            project.dependencies.add("implementation", jfxDep)
+        }
+
     }
 
 }
