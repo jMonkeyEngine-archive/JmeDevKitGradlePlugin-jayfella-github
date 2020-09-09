86b9dc5c79d645e11f02ad16043766c489367184
jayfella <jameskhan1983@gmail.com>
2020-09-09 13:47:16 +0100

    bump version

--- src/main/groovy/com/jayfella/devkit/gradle/JmeDevKitGradlePlugin.groovy
+++ src/main/groovy/com/jayfella/devkit/gradle/JmeDevKitGradlePlugin.groovy
@@ -73,7 +73,7 @@ class JmeDevKitGradlePlugin implements Plugin<Project> {
     private void configureDependencies() {
 
         // include the devkit as a dependency
-        Dependency dep = project.dependencies.create("com.jayfella:jme-swing-devkit:1.0.5")
+        Dependency dep = project.dependencies.create("com.jayfella:jme-swing-devkit:1.0.6")
         project.dependencies.add("runtimeOnly", dep)
 
     }
